937a80efd1ad938feaa35d49dc3b94633a8e321a
Jonathan Taylor <jonathan.taylor@datavant.com>
2025-07-15 16:51:36 -0600

    install nerdfont with script

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo "creating symlink from ${SCRIPTPATH}/neovim/.config/nvim to ${HOME}/.config/nvim"
 ln -s "${SCRIPTPATH}/neovim/.config/nvim" "${HOME}/.config/nvim"
+
+curl https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip -O -J -L
+unzip JetBrainsMono
+mv *.ttf ~/Library/Fonts/
+rm JetBrainsMono.zip OFL.txt README.md
